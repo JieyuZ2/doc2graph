4b1f50c0e67d832dece07e52d869b3614359b5eb
jieyuz2 <jieyuz2@illinosi.edu>
2020-02-20 15:53:51 -0600

    current best

--- run.sh
+++ run.sh
@@ -2,14 +2,6 @@
 
 data=$1
 
-python3 src/main.py --dataset ${data} --model NetGen --teacher_forcing 1 --recon_weight 1.0 --tag "11n"
-python3 src/main.py --dataset ${data} --model NetGen --teacher_forcing 1 --recon_weight 0.0 --tag "10n"
-
-python3 src/main.py --dataset ${data} --model NetGenWord --teacher_forcing 1 --recon_weight 1.0 --tag "11n"
-python3 src/main.py --dataset ${data} --model NetGenWord --teacher_forcing 1 --recon_weight 0.0 --tag "10n"
-
-python3 src/main.py --dataset ${data} --model NetGenS --teacher_forcing 1 --recon_weight 1.0 --tag "11n"
-python3 src/main.py --dataset ${data} --model NetGenS --teacher_forcing 1 --recon_weight 0.0 --tag "10n"
-
-python3 src/main.py --dataset ${data} --model NetGenWordS --teacher_forcing 1 --recon_weight 1.0 --tag "11n"
-python3 src/main.py --dataset ${data} --model NetGenWordS --teacher_forcing 1 --recon_weight 0.0 --tag "10n"
+python3 src/main.py --dataset ${data} --model NetGen --teacher_forcing 1
+python3 src/main.py --dataset ${data} --model NetGenWord --teacher_forcing 1
+python3 src/main.py --dataset ${data} --model NetGenLink --teacher_forcing 1
